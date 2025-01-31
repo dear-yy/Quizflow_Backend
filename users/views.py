3a95d2c95aa9273a1de7f0612cf65eed8c09e50b
Yoonseo <yoonseo1206@gmail.com>
2025-02-01 02:43:43 +0900

    APIView 임포트

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import generics,status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from .permissions import CustomReadOnly
 from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
 from django.contrib.auth.models import User
